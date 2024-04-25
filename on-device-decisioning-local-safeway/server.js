4b40b9907e0ca3432a42957b2bc1f30068f7d134
Nishtha <nishthag@adobe.com>
2024-04-25 13:35:40 -0600

    renaming file names

--- on-device-decisioning-local-safeway/server.js
+++ on-device-decisioning-local-safeway/server.js
@@ -123,7 +123,7 @@ function startExpressApp() {
   process.on('SIGTERM', () => {
     console.log('SIGTERM signal received: closing HTTP server');
     server.close(() => {
-      console.log('HTTP server closed');
+      console.log('HTTP server close');
     });
   });
 }
